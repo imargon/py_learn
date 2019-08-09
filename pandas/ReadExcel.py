2b3a8a1001f6693ab9cd702332738048acc2e0f7
imargon <skypz666@gmail.com>
2019-08-09 23:40:02 +0800

    Update ReadExcel.py

--- pandas/ReadExcel.py
+++ pandas/ReadExcel.py
@@ -1,3 +1,19 @@
-#!/usr/bin/python3
-import pandas as pd
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
 import numpy as np
+import pandas as pd
+
+file_path = 'F:/py/list.xlsx'
+
+
+def read_file(file_path):
+    df = pd.read_excel(file_path, sheet_name='Sheet1')
+    df = df.groupby('Official level')['amount'].sum()
+    #file_dat = df.head()  # 默认读取前5行的数据
+    print("获取到所有的值:\n{0}".format(df))  # 格式化输出
+
+
+if __name__ == '__main__':
+    read_file(file_path)
