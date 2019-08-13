f1bc586fdd582c7c82077f08d2a461a39661de37
imargon <skypz666@gmail.com>
2019-08-13 22:30:32 +0800

    Update opencv.py

--- image/opencv.py
+++ image/opencv.py
@@ -56,3 +56,34 @@ def run():
 
 if __name__ == "__main__":
     run()    
+
+#https://docs.opencv.org/3.3.0/da/d6e/tutorial_py_geometric_transformations.html   
+    
+import cv2
+import numpy as np
+img = cv2.imread('messi5.jpg')
+res = cv2.resize(img,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+#OR
+height, width = img.shape[:2]
+res = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+
+import cv2
+import numpy as np
+img = cv2.imread('messi5.jpg',0)
+rows,cols = img.shape
+M = np.float32([[1,0,100],[0,1,50]])
+dst = cv2.warpAffine(img,M,(cols,rows))
+cv2.imshow('img',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+img = cv2.imread('sudoku.png')
+rows,cols,ch = img.shape
+pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+M = cv2.getPerspectiveTransform(pts1,pts2)
+dst = cv2.warpPerspective(img,M,(300,300))
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
+plt.show()
